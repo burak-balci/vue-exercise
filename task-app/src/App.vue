6bc2d9df6f1b88be0b69bf3739b68ff5be69403d
Burak <96511762+burak-balci@users.noreply.github.com>
2022-11-10 09:43:04 +0300

    accessing store in pinia

--- task-app/src/App.vue
+++ task-app/src/App.vue
@@ -1,5 +1,28 @@
-<template></template>
-
 <script>
-export default {};
+import { useTaskStore } from "./stores/TaskStore";
+import TaskDetails from "./components/TaskDetails.vue";
+
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore();
+
+    return { taskStore };
+  },
+};
 </script>
+
+<template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+  </main>
+</template>
---
+++ task-app/src/stores/TaskStore.js
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export const useTaskStore = defineStore("taskStore", {
+  state: () => ({
+    tasks: [
+      { id: 1, title: "buy some milk", isFav: false },
+      { id: 2, title: "play hs", isFav: true },
+    ],
+  }),
+});
